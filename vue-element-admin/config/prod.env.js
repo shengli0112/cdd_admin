e22b49281eaae4291aa0f8d4cbdf9ea7e4cd9876
shihui.he <shihui.he@qunar.com>
2019-06-24 22:51:25 +0800

    dd

--- vue-element-admin/config/prod.env.js
+++ vue-element-admin/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"https://api-prod"',
-    APP_ORIGIN: '"https://wallstreetcn.com"'
+    BASE_API: '"http//admin.cddwang.com/admin"',
+    APP_ORIGIN: '"http//admin.cddwang.com/admin"'
 };
