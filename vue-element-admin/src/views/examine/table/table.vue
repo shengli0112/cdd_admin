cd8a0149d24a08386e184bf974999432de4a17a9
guoshengli <guoshengli@sunlands.com>
2019-10-11 18:56:28 +0800

    dd

--- vue-element-admin/src/views/examine/table/table.vue
+++ vue-element-admin/src/views/examine/table/table.vue
@@ -157,7 +157,7 @@
     </el-table>
 
     <div v-show="!listLoading" class="pagination-container">
-      <el-pagination v-if="paginationShow" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
         :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total" :ref="pagination">
       </el-pagination>
     </div>
