cc98b60ee6f882cb16810befff0f1acf25e7ab32
guoshengli <guoshengli@sunlands.com>
2019-11-19 19:11:47 +0800

    用户资源收集bug

--- vue-element-admin/src/views/user_broker/table/table.vue
+++ vue-element-admin/src/views/user_broker/table/table.vue
@@ -177,7 +177,7 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogUploadVisible = false">取 消</el-button>
-        <el-button v-else type="primary" @click="submitUpload">确 定</el-button>
+        <el-button type="primary" @click="submitUpload">确 定</el-button>
       </div>
     </el-dialog>
 
